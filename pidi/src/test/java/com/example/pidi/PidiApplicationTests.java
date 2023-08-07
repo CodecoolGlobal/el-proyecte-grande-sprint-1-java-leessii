d3ddf89114e1c558c40d0574647ce542aecd6361
leessii <ursa.lesnik@gmail.com>
2023-08-07 12:11:39 +0200

    test

---
+++ pidi/src/test/java/com/example/pidi/PidiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pidi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PidiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
