2f25aa28783fe7b4f10eebdc16bedd22ec4c4d31
leessii <ursa.lesnik@gmail.com>
2023-08-22 13:39:35 +0200

    ignore idea folder

---
+++ pidiServer/src/main/resources/data-postgresql.sql
@@ -0,0 +1,5 @@
+INSERT INTO adoption_status (status) VALUES ('Available');
+INSERT INTO adoption_status (status) VALUES ('In Progress');
+INSERT INTO adoption_status (status) VALUES ('Not Avaliable');
+INSERT INTO adoption_status (status) VALUES ('Adoppted');
+
