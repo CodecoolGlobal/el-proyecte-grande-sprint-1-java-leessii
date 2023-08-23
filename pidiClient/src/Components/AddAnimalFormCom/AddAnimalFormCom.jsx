71a88236bf557578fcd89ccfffaaead141c2cbc5
leessii <ursa.lesnik@gmail.com>
2023-08-23 14:57:59 +0200

    form works but the table not

--- pidiClient/src/Components/AddAnimalFormCom/AddAnimalFormCom.jsx
+++ pidiClient/src/Components/AddAnimalFormCom/AddAnimalFormCom.jsx
@@ -1,16 +1,93 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {
+  Button,
   FormControl,
-  InputAdornment,
+  FormGroup,
   InputLabel,
-  OutlinedInput,
-  Paper,
+  MenuItem,
+  Select,
 } from '@mui/material'
+import TextField from '@mui/material/TextField'
+import {useMutation} from '@tanstack/react-query'
+import AnimalsApi from '../../Api/AnimalsApi'
+import {DatePicker, LocalizationProvider} from '@mui/x-date-pickers'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
-function AddAnimalFormCom() {
+
+
+function AddAnimalFormCom({adoptionStatus}) {
+  const [data, setData] = useState({adoptionStatus: '', admissionDate: null})
+  const updateData = (value) => setData(data => ({...data, ...value}))
+
+  const saveAnimalMutation = useMutation(async (newData) => {
+    const response = await AnimalsApi.saveAnimal(newData)
+    return response.data
+  })
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    const formData = new FormData(event.target)
+    const newData = {
+      name: formData.get('name'),
+      age: formData.get('age'),
+      species: formData.get('species'),
+      adoptionStatus: data.adoptionStatus,
+      admissionDate: data.admissionDate
+    }
+
+    try {
+      console.log(newData)
+      await saveAnimalMutation.mutateAsync(newData)
+    } catch (error) {
+      // Handle error
+    }
+  }
+
+
   return (
-    <form>
-      <Paper>
+    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+      <form onSubmit={handleSubmit} style={{width: '50%'}}>
+        <FormGroup sx={{padding: 2, borderRadius: 2, border: '1px solid', borderColor: 'primary.main'}}>
+          <TextField sx={{paddingBottom: 2}} name="name" variant="outlined" placeholder="Name..."/>
+          <TextField type="number" sx={{paddingBottom: 2}} name="age" variant="outlined" placeholder="Age..."/>
+          <TextField sx={{paddingBottom: 2}} name="species" variant="outlined" placeholder="Speccies..."/>
+          <FormControl fullWidth sx={{paddingBottom: 2}}>
+            <InputLabel id="adoptionStatus">Adoption Status</InputLabel>
+            <Select
+              labelId="adoptionStatus"
+              id="adoptionStatus"
+              label="Adoption Status"
+              value={data.adoptionStatus}
+              onChange={(e) => updateData({ adoptionStatus: e.target.value })}
+            >
+              {adoptionStatus.map((status) => (
+                <MenuItem key={status.id} value={status}>
+                  {status.status}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl fullWidth sx={{paddingBottom: 2}}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                value={data.admissionDate}
+                onChange={(newDate) => updateData({admissionDate: newDate})}
+              />
+            </LocalizationProvider>
+          </FormControl>
+          <Button type="submit" variant="outlined">Submit</Button>
+        </FormGroup>
+      </form>
+    </div>
+  )
+}
+
+export default AddAnimalFormCom
+
+
+/*
+<Paper>
         <FormControl sx={{m: 1, width: '25ch'}}>
           <InputLabel htmlFor="name">Name</InputLabel>
           <OutlinedInput
@@ -35,8 +112,4 @@ function AddAnimalFormCom() {
           />
         </FormControl>
       </Paper>
-    </form>
-  )
-}
-
-export default AddAnimalFormCom
+ */
--- pidiClient/src/Pages/AddAnimal.jsx
+++ pidiClient/src/Pages/AddAnimal.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import NavbarCom from '../Components/NavbarCom/NavbarCom'
-import {Button} from '@mui/material'
+import {Button, CircularProgress} from '@mui/material'
 import {Link} from 'react-router-dom';
 import AddAnimalFormCom from '../Components/AddAnimalFormCom/AddAnimalFormCom'
+import {useQuery} from '@tanstack/react-query'
+import AdoptionStatusApi from '../Api/AdoptionStatusApi'
 
 const customButtons = [
   <Button key="allAnimlas" color="inherit" component={Link} to="/admin">
@@ -11,10 +13,15 @@ const customButtons = [
 ]
 
 function AddAnimal() {
+  const getAdoptionStatusQuery = useQuery({
+    queryKey: ['adoptionStatus'],
+    queryFn: () => AdoptionStatusApi.get(),
+  })
+
   return (
     <div>
       <NavbarCom customButtons={customButtons}/>
-      <AddAnimalFormCom />
+      {getAdoptionStatusQuery.isLoading ? <CircularProgress/> : <AddAnimalFormCom adoptionStatus={getAdoptionStatusQuery.data} />}
     </div>
   )
 }
