1baa99f09e9d48c664efaa9d12abba35d5b28bab
leessii <ursa.lesnik@gmail.com>
2023-09-05 10:08:02 +0200

    application exception handler added

--- pidiServer/src/main/java/com/example/pidi/controller/exeption/ApplicationExceptionHandler.java
+++ pidiServer/src/main/java/com/example/pidi/controller/exeption/ApplicationExceptionHandler.java
@@ -3,14 +3,17 @@ package com.example.pidi.controller.exeption;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
+@RestControllerAdvice
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class ExceptionHandler extends Exception {
+public class ApplicationExceptionHandler extends Exception {
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     public Map<String, String> handleValidationExceptions(
             MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
