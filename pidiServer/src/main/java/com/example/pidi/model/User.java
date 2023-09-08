1c50aff917a050ff60fcdbd9006ce616a2f4c5ae
leessii <ursa.lesnik@gmail.com>
2023-09-08 11:38:57 +0200

    log in

--- pidiServer/src/main/java/com/example/pidi/model/User.java
+++ pidiServer/src/main/java/com/example/pidi/model/User.java
@@ -1,2 +1,26 @@
-package com.example.pidi.model;public class User {
+package com.example.pidi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
 }
